7a03196184c6184db0c0c4889adaa18bb880c296
wuguanchao <wugc1998@163.com>
2022-11-07 17:21:37 +0800

    chore: lua

---
+++ lua/plugins.lua
@@ -0,0 +1,28 @@
+local packer = require("packer")
+
+packer.startup({
+  function(use) 
+    use ("wbthomason/packer.nvim")
+    use("ful1e5/onedark.nvim")
+    use ({"nvim-tree/nvim-tree.lua", requires = {"nvim-tree/nvim-web-devicons"}})
+    use({ "akinsho/bufferline.nvim", requires = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" }})
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float({ border = "single" })
+      end
+    }
+  }
+})
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
